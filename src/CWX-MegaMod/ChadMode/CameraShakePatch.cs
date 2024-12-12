2bf8a458f5155faa3583e1056e88a91e63b4f901
CWX <cwxdev@outlook.com>
2024-12-12 00:09:53 +0000

    add remove camera shake on hit effect

---
+++ src/CWX-MegaMod/ChadMode/CameraShakePatch.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+
+namespace CWX_MegaMod.ChadMode
+{
+    public class CameraShakePatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(EffectsController), nameof(EffectsController.method_7));
+        }
+
+        [PatchPrefix]
+        public static bool PatchPrefix()
+        {
+            if (MegaMod.CameraShake.Value)
+            {
+                // Skip method
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
