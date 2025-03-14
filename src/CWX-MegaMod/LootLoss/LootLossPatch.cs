cb87d6bc3bd9039528c77aea126ee36b2d51cb8a
CWX <cwxdev@outlook.com>
2025-03-14 13:20:41 +0000

    add option to bypass loot gen

---
+++ src/CWX-MegaMod/LootLoss/LootLossPatch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using EFT;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+
+namespace CWX_MegaMod.LootLoss
+{
+    public class LootLossPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(LocalGame), nameof(LocalGame.smethod_6));
+        }
+
+        [PatchPrefix]
+        public static void PatchPrefix(ref LocalRaidSettings raidSettings)
+        {
+            if (MegaMod.LootLoss.Value)
+            {
+                raidSettings.selectedLocation.containers = new Dictionary<string, LocationSettingsClass.Location.GClass1350>();
+                raidSettings.selectedLocation.Loot = new GClass1333();
+            }
+        }
+    }
+}
